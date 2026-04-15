5c2f5a0dd1fe01e54f711e86f6519c060a035df4
SylvanoMiary <sylvanobernardinv@gmail.com>
2026-04-15 16:46:41 +0300

    sunny v2

--- lib/services/sunny_service.dart
+++ lib/services/sunny_service.dart
@@ -5,7 +5,7 @@ import 'package:sunnypool_app/models/message_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
-/* Future<XFile?> compressImage(String path) async {
+Future<XFile?> compressImage(String path) async {
   final result = await FlutterImageCompress.compressAndGetFile(
     path,
     path + "_compressed.jpg",
@@ -15,54 +15,18 @@ import 'package:image_picker/image_picker.dart';
     return XFile(result.path);
   }
   return null;
-} */
+}
 
 class SunnyService {
-  static const String baseUrl =
-      "https://sunny.trouvezpourmoi.com/wp-json/sunny-pool/v1";
-
-  Future<String?> _toBase64IfFilePath(String? value) async {
-    if (value == null || value.isEmpty) return null;
-
-    final file = File(value);
-    if (await file.exists()) {
-      final bytes = await file.readAsBytes();
-      return base64Encode(bytes);
-    }
-
-    return value;
-  }
+  static const String baseUrl = "https://n8n.trouvezpourmoi.com/webhook-test/sunny";
+  //"https://sunny.trouvezpourmoi.com/wp-json/sunny-pool/v1";
 
   Future<Map<String, dynamic>> sendChat(
-    String token,
     String sessionId,
     MessageModel message,
   ) async {
     print(sessionId);
-
-    final photoBase64 = await _toBase64IfFilePath(message.image);
-
-    final response = await http.post(
-      Uri.parse("$baseUrl/chat"),
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer $token",
-      },
-      body: jsonEncode({
-        "message": message.message,
-        "image_base64": photoBase64,
-        "conversation_id": sessionId,
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      return data;
-    } else {
-      throw Exception("Erreur de connexion : \\${response.body}");
-    }
-
-    /* var uri = Uri.parse("$baseUrl/v2");
+    var uri = Uri.parse("$baseUrl/v2");
 
     late http.Response response;
 
@@ -99,7 +63,7 @@ class SunnyService {
       return data[0];
     } else {
       throw Exception("Erreur de connexion : \\${response.body}");
-    } */
+    }
   }
 
   Future<Map<String, dynamic>> responseChat(
